eca856991b858fc5a9793548f7a3bb78599b2576
Sergii Makagon <makagon87@gmail.com>
2017-06-06 09:29:07 -0400

    Switched form Faraday to net/http

--- eventify.gemspec
+++ eventify.gemspec
@@ -3,6 +3,7 @@
 
 lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'eventify'
 require 'eventify/version'
 
 Gem::Specification.new do |spec|
@@ -23,15 +24,13 @@ Gem::Specification.new do |spec|
       'public gem pushes.'
   end
 
-  spec.files = `git ls-files -z`.split("\x0").reject do |f|
-    f.match(%r{^(test|spec|features)/})
-  end
+  spec.files = ['lib/eventify.rb', 'lib/eventify/version.rb']
+  spec.test_files = `git ls-files -- {test,spec,features}/*`.split("\n")
   spec.bindir = 'bin'
   spec.require_paths = ['lib']
 
   spec.add_development_dependency 'bundler', '~> 1.15'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
-  spec.add_development_dependency 'rubocop'
-  spec.add_development_dependency 'faraday'
+  spec.add_development_dependency 'rubocop', '~> 0.49.1'
 end
--- lib/eventify.rb
+++ lib/eventify.rb
@@ -1,8 +1,52 @@
 # frozen_string_literal: true
 
+require 'net/http'
+require 'uri'
 require 'eventify/version'
-require 'eventify/client'
 
 # Eventify allows to publish events from Ruby applications
 module Eventify
+  EventifyError = Class.new(StandardError)
+  InvalidTokenError = Class.new(EventifyError)
+  ServiceUnavailable = Class.new(EventifyError)
+
+  # Client that allows to publish events
+  class Client
+    def initialize(api_key: nil, logger: nil, raise_error: false)
+      @api_key = api_key || ENV['EVENTIFY_API_KEY']
+      @logger = logger
+      @raise_error = raise_error
+    end
+
+    def publish(type, data)
+      events_url = "#{base_uri}/events"
+
+      uri = URI.parse(events_url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Post.new(uri.path, headers)
+      request.set_form_data(type: type, data: data)
+      response = http.request(request)
+
+      puts response.body.inspect
+    end
+
+    private
+
+    attr_reader :api_key, :logger, :raise_error
+
+    def http_client
+      @http_client || Faraday
+    end
+
+    def headers
+      {
+        'Authorization' => 'secret_token',
+        'Content-Type' => 'application/json; charset=utf-8'
+      }
+    end
+
+    def base_uri
+      'http://localhost:3000'
+    end
+  end
 end
